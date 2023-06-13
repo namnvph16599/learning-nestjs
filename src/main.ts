7a25f1f53246a03a258be4937523995b39db4513
Ngô Vưn Nam <anh02284@gmail.com>
2023-06-13 23:38:29 +0700

    connect db

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const PORT = 8080;
+  const PORT = 3000;
   const app = await NestFactory.create(AppModule);
   console.log('SERVER RUNNING PORT: ' + PORT);
 

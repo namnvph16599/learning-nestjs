7a216b3b8127989b20759d2faa6e2fab16be3ee1
Ngô Vưn Nam <anh02284@gmail.com>
2023-06-14 23:34:57 +0700

    feat: even entity

---
+++ src/modules/events/event.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Event {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  schedule: Date;
+}
